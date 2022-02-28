d4c84004b2ee9ad58a34b087821bf585e0315cf1
vladimiremi <vladimiremi2014@gmail.com>
2022-02-28 12:01:41 -0300

    feat: add TopBar component

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,24 @@
+import { Avatar, Box, Flex } from "@chakra-ui/react";
+import { Logo } from "assets/icons/icons";
+
+export function TopBar() {
+  return (
+    <Flex
+      w="100%"
+      border="2px solid #EAEFED"
+      justifyContent="space-between"
+      bg="#FFFFFF"
+    >
+      <Box p="4" borderRight="2px solid #EAEFED" px="7">
+        <Logo />
+      </Box>
+      <Flex borderLeft="2px solid #EAEFED" pl="2" p="4">
+        <Avatar bg="#B5F1DD" color="" name="Luiz Zlochevsky" mx="2" />
+        <div>
+          <h4>Luiz Zlochevsky</h4>
+          <text>meus dados</text>
+        </div>
+      </Flex>
+    </Flex>
+  );
+}
